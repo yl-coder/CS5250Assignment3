07ae7f2110d8380b7057ee3be29bfdb237f2e5d0
You Liang Lim <a0125975@u.nus.edu>
2018-04-01 23:28:17 +0800

    finished read method

--- one.c
+++ one.c
@@ -31,7 +31,13 @@ int onebyte_release(struct inode * inode, struct file * filep) {
   return 0; // always successful
 }
 ssize_t onebyte_read(struct file * filep, char * buf, size_t count, loff_t * f_pos) {
-  /*please complete the function on your own*/
+  copy_to_user(buf, onebyte_data, 1);
+
+  if (*f_pos == 0) { 
+    *f_pos += 1;
+  } 
+  
+  return (*f_pos == 0) ? 1 : 0;
 }
 ssize_t onebyte_write(struct file * filep,
   const char * buf,
